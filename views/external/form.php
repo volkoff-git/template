9752e1c5dcb02a629660995ada655ca01d0b0f30
volkoff <rock.n.yosh@gmail.com>
2024-07-25 17:50:51 +0300

    external form

--- views/external/form.php
+++ views/external/form.php
@@ -1,3 +1,65 @@
 анкета
 
 <? var_export($params); ?>
+
+
+<div class="card mb-4">
+	<div class="ribbon bg-red">Необходимо заполнить</div>
+	<div class="card-body">
+		<h3 class="card-title">Данные для одобрения</h3>
+
+
+		<div class="row g-5">
+			<div class="col-lg-4 col-md-6">
+				<div class="mb-3"><label class="form-label">Text</label><input type="text" class="form-control" name="example-text-input" placeholder="Input placeholder"></div>
+				<div class="mb-3"><label class="form-label">Text</label><input type="text" class="form-control" name="example-text-input" placeholder="Input placeholder"></div>
+			</div>
+
+
+
+			<div class="col-lg-4 col-md-6">
+				<div class="mb-3"><label class="form-label">Text</label><input type="text" class="form-control" name="example-text-input" placeholder="Input placeholder"></div>
+				<div class="mb-3"><label class="form-label">Text</label><input type="text" class="form-control" name="example-text-input" placeholder="Input placeholder"></div>
+			</div>
+
+
+			<div class="col-lg-4 col-md-6">
+				<div class="mb-3"><label class="form-label">Text</label><input type="text" class="form-control" name="example-text-input" placeholder="Input placeholder"></div>
+				<div class="mb-3"><label class="form-label">Text</label><input type="text" class="form-control" name="example-text-input" placeholder="Input placeholder"></div>
+			</div>
+		</div>
+
+	</div>
+</div>
+
+
+<div class="card mb-4">
+	<div class="ribbon bg-green">заполнено</div>
+	<div class="card-body">
+		<h3 class="card-title">Данные для выдачи</h3>
+
+
+		<div class="row g-5">
+			<div class="col-lg-4 col-md-6">
+				<div class="mb-3"><label class="form-label">Text</label><input type="text" class="form-control" name="example-text-input" placeholder="Input placeholder"></div>
+				<div class="mb-3"><label class="form-label">Text</label><input type="text" class="form-control" name="example-text-input" placeholder="Input placeholder"></div>
+			</div>
+
+
+
+			<div class="col-lg-4 col-md-6">
+				<div class="mb-3"><label class="form-label">Text</label><input type="text" class="form-control" name="example-text-input" placeholder="Input placeholder"></div>
+				<div class="mb-3"><label class="form-label">Text</label><input type="text" class="form-control" name="example-text-input" placeholder="Input placeholder"></div>
+			</div>
+
+
+			<div class="col-lg-4 col-md-6">
+				<div class="mb-3"><label class="form-label">Text</label><input type="text" class="form-control" name="example-text-input" placeholder="Input placeholder"></div>
+				<div class="mb-3"><label class="form-label">Text</label><input type="text" class="form-control" name="example-text-input" placeholder="Input placeholder"></div>
+			</div>
+		</div>
+
+	</div>
+</div>
+
+
